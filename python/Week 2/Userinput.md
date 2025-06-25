fa3f38deff8c23caa5aed5c5961c10fd1159c70d
Ja'Den Traylor <jaden.traylor@hgs.hiddengeniusproject.org>
2025-06-25 14:00:25 -0700

    Exit ticket week 2

---
+++ python/Week 2/Userinput.md
@@ -0,0 +1,7 @@
+1. What function do we use to get input from the user?
+The input function.
+
+2. What data type does the input() function return? The string data type.
+
+
+3. Give an example of a software that requires user input to work. A software that that requires input is mircosoft.
---
+++ python/Week 2/Userinput.py
@@ -0,0 +1,10 @@
+name = input('Hi, I am Pyton! what is your name?')
+print("Hello," + name + "it's nice to meet you")
+ 
+age = input("How old are you?")
+print("That's nice, I am 23 in college")
+
+grade = input("What grade are you in")
+print("Wait till you get to collge it's more free")
+      
+      
