abe19c894e8252c75c1857ca5804128913f80f67
Ja'Den Traylor <jaden.traylor@hgs.hiddengeniusproject.org>
2025-06-26 14:04:52 -0700

    QuikMathexit

---
+++ python/Week 2/QuikMath2.md
@@ -0,0 +1,12 @@
+1. Define what a function is in your own words
+A function is a command you give.
+
+2. What key word is used to define a function? 
+Def Keyword
+
+3. What does a return statement do? T
+he return data the function returns when complete.
+
+4. What is the difference between 
+arguments and parameters?
+Parmeters are the named placeholders and arguments are the real vaules you pass.
---
+++ python/Week 2/QuikMath2.py
@@ -0,0 +1,22 @@
+def Add(addend1, addend2):
+    return addend1 + addend2
+
+def Subtract(subtractend1, subtractend2):
+    return subtractend1 - subtractend2
+
+def Multiply(multiply1, multiply2):
+    return multiply1 * multiply2
+
+def Divde(divide1, divide2):
+    return divide1 / divide2
+
+
+sum = Add(5, 35)
+difference = Subtract(10,90)
+double = Multiply(12, 9)
+take = Divde(144, 12)
+
+print(sum)
+print(difference)
+print(double)
+print(take)
