0f3a19e0b5663c906e56df553fbfb2022bdaf74b
Ja'Den Traylor <jaden.traylor@hgs.hiddengeniusproject.org>
2025-07-10 14:31:05 -0700

    html

---
+++ python/week4/DataScenarios.py
@@ -0,0 +1,14 @@
+print("Snecrio #1: A restaurant menu with prices for each item")
+print("Best Structure: Dictionary; best to pair good with price in key/value")
+menu = {
+    "French Toast":12,
+    "Grand Slam": 12,
+    "T-Bone Steak": 18,
+    "Avacado Toast":15
+}
+for key, item in menu.items():
+    print(key,":", item)
+
+
+print("\nScenario #2: High scores to an arcade game")
+print 
